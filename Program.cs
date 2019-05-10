7a58f5b63a1bec8f7bc107a04717b5a5de393818
czainski <38532082+czainski@users.noreply.github.com>
2019-05-10 10:30:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skwark1
+{
+    class Program
+    {
+        //język C#
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hubert");
+            Console.ReadKey();
+            Console.WriteLine("Wałbrzych");
+            Console.ReadKey();
+        }
+    }
+}
